81378fa7f27f6050beea37033bf1fd1f37f7858a
algoreddy <helloyash99@gmail.com>
2025-10-01 14:51:05 +0530

    first commit with initial project setup

---
+++ src/test/java/net/algoreddy/JournalApp/JournalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.algoreddy.JournalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
