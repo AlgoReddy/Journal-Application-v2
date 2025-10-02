b6c5a54367adbbef697d2f026d0f3b4e3797bb66
algoreddy <helloyash99@gmail.com>
2025-10-02 23:25:52 +0530

    implemented the lombok annos & making the code cleaner & easier to read & debug

--- src/main/java/net/algoreddy/JournalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/net/algoreddy/JournalApp/controller/JournalEntryControllerV2.java
@@ -4,15 +4,14 @@ import net.algoreddy.JournalApp.Service.JournalEntryService;
 import net.algoreddy.JournalApp.entity.JournalEntry;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 //package net.algoreddy.JournalApp.controller;
 
 import net.algoreddy.JournalApp.entity.JournalEntry;
@@ -35,22 +34,34 @@ public class JournalEntryControllerV2 {
     }
 
     @PostMapping
-    public JournalEntry createEntry(@RequestBody JournalEntry myEntry) {
-        myEntry.setDate(LocalDateTime.now());
-        journalEntryService.saveEntry(myEntry);
-        return myEntry;
+    public  ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry) {
+       try {
+           myEntry.setDate(LocalDateTime.now());
+           journalEntryService.saveEntry(myEntry);
+//        return myEntry;
+           return  new ResponseEntity<>(myEntry , HttpStatus.ACCEPTED);
+       }catch (Exception e){
+           return  new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+       }
 
     }
 
     @GetMapping("id/{myId}")
-    public JournalEntry getJournalEntryById(@PathVariable ObjectId myId) {
-        return journalEntryService.findById(myId).orElse(null);
+    public ResponseEntity<JournalEntry> getJournalEntryById(@PathVariable ObjectId myId) {
+        Optional<JournalEntry> journalEntry = journalEntryService.findById(myId);
+
+//        return journalEntry.map(entry -> new ResponseEntity<>(entry, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+////        }
+        if (journalEntry.isPresent()) {
+            return new ResponseEntity<>(journalEntry.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("id/{myId}")
-    public boolean deleteJournalEntryById(@PathVariable ObjectId myId) {
+    public ResponseEntity<?> deleteJournalEntryById(@PathVariable ObjectId myId) {
         journalEntryService.deleteById(myId);
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("/id/{myId}")
