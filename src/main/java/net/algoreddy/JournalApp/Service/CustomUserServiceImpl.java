312e55f2057495dfd53616b103ee443b06cad353
algoreddy <helloyash99@gmail.com>
2025-10-07 14:44:15 +0530

    till now implemented the springsecurity &BcryptpasswordEncode along with the authentication & UserServiceIMpl

---
+++ src/main/java/net/algoreddy/JournalApp/Service/CustomUserServiceImpl.java
@@ -0,0 +1,28 @@
+package net.algoreddy.JournalApp.Service;
+
+import net.algoreddy.JournalApp.entity.User;
+import net.algoreddy.JournalApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        if (username != null) {
+            UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
+                    .username(user.getUserName())
+                    .password(user.getPassword())
+                    .roles(user.getRoles().toArray(new String[0]))
+                    .build();
+            return userDetails;
+        }
+        throw new UsernameNotFoundException("Username not found with the username"+username );
+    }
+}
