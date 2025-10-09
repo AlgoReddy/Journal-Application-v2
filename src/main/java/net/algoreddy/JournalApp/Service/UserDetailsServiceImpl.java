556e08ca20a9856bcb99e26dceef15addacf3804
algoreddy <helloyash99@gmail.com>
2025-10-09 22:24:35 +0530

    completed the creation of endpoints & authentication to the users using springSecurity

--- src/main/java/net/algoreddy/JournalApp/Service/UserDetailsServiceImpl.java
+++ src/main/java/net/algoreddy/JournalApp/Service/UserDetailsServiceImpl.java
@@ -3,25 +3,26 @@ package net.algoreddy.JournalApp.Service;
 import net.algoreddy.JournalApp.entity.User;
 import net.algoreddy.JournalApp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
-public class CustomUserServiceImpl implements UserDetailsService {
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUserName(username);
-        if (username != null) {
-            UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
+        if (user!= null) {
+            // it's an inline function learn more about the inline function
+            return org.springframework.security.core.userdetails.User.builder()
                     .username(user.getUserName())
                     .password(user.getPassword())
                     .roles(user.getRoles().toArray(new String[0]))
                     .build();
-            return userDetails;
         }
         throw new UsernameNotFoundException("Username not found with the username"+username );
     }
--- src/main/java/net/algoreddy/JournalApp/controller/UserController.java
+++ src/main/java/net/algoreddy/JournalApp/controller/UserController.java
@@ -2,9 +2,12 @@ package net.algoreddy.JournalApp.controller;
 
 import net.algoreddy.JournalApp.Service.UserService;
 import net.algoreddy.JournalApp.entity.User;
+import net.algoreddy.JournalApp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,7 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
+    @Autowired
+    private UserRepository userRepository;
     @Autowired
     private UserService userService;
 
@@ -21,13 +25,12 @@ public class UserController {
         return userService.getAll();
     }
 
-    @PostMapping
-    public void createUser(@RequestBody User user) {
-        userService.saveEntry(user);
-    }
 
-    @PutMapping("/{userName}")
-    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String userName) {
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user) {
+        // securitycontextholder . . . --> it make sure that the user is authenticated by checking the password &user avaliable in the database using the hasing
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
         User userInDb = userService.findByUserName(userName);
         if (userInDb != null) {
             userInDb.setUserName(user.getUserName());
@@ -36,4 +39,40 @@ public class UserController {
         }
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+    @DeleteMapping
+    public ResponseEntity<?>deleteUser(@RequestBody User user){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        userRepository.deleteByuserName(authentication.getName());
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+//    @PutMapping
+//    public ResponseEntity<?> updateUser(@RequestBody User user) {
+//        // Get the currently authenticated user
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        String loggedInUserName = authentication.getName();
+//
+//        // Find user in DB
+//        User userInDb = userService.findByUserName(loggedInUserName);
+//        if (userInDb == null) {
+//            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+//        }
+//
+//        // Update only allowed fields
+//        if (    !user.getUserName().isBlank()) {
+//            userInDb.setUserName(user.getUserName());
+//        }
+//
+//        if (user.getPassword() != null && !user.getPassword().isBlank()) {
+//            // Re-encode the password before saving!
+//            userInDb.setPassword(UserService.passwordEncoder.encode(user.getPassword()));
+//        }
+//
+//        // Save updated user
+//        userService.saveEntry(userInDb);
+//
+//        return new ResponseEntity<>("User updated successfully", HttpStatus.OK);
+//    }
+
 }
