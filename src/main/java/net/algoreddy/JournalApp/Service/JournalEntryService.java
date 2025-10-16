e0c7b6bed1c90bfc9b747a3a37ad2fb29f472f31
algoreddy <helloyash99@gmail.com>
2025-10-16 23:02:53 +0530

    implemented the sonalint capability to increase the code maintainability & security

--- src/main/java/net/algoreddy/JournalApp/Service/JournalEntryService.java
+++ src/main/java/net/algoreddy/JournalApp/Service/JournalEntryService.java
@@ -1,9 +1,13 @@
 package net.algoreddy.JournalApp.Service;
 
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import net.algoreddy.JournalApp.entity.JournalEntry;
 import net.algoreddy.JournalApp.entity.User;
 import net.algoreddy.JournalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,12 +16,16 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+
 @Component
+@Slf4j
 public class JournalEntryService {
     @Autowired
     private JournalEntryRepository journalEntryRepository;
     @Autowired
     private UserService userService;
+//    private static final Logger logger = LoggerFactory.getLogger(JournalEntryService.class);
+
 
     @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName) {
@@ -51,14 +59,14 @@ public class JournalEntryService {
         boolean removed;
         try {
             User user = userService.findByUserName(userName);
-             removed = user.getJournalEntries().removeIf(x -> x.getId().equals(id));
+            removed = user.getJournalEntries().removeIf(x -> x.getId().equals(id));
             if (removed) {
 
                 userService.saveUser(user);
                 journalEntryRepository.deleteById(id);
             }
         } catch (Exception e) {
-            System.out.println(e);
+            log.error("Error" , e );
             throw new RuntimeException("An error occured while deleting the entry");
         }
         return removed;
--- src/main/java/net/algoreddy/JournalApp/Service/UserService.java
+++ src/main/java/net/algoreddy/JournalApp/Service/UserService.java
@@ -1,8 +1,11 @@
 package net.algoreddy.JournalApp.Service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.algoreddy.JournalApp.entity.User;
 import net.algoreddy.JournalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,25 +16,35 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
     public static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
+//    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+
     public void saveUser(User user) {
         userRepository.save(user);
     }
 
     public boolean saveNewUser(User user) {
-        try{
+        try {
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             user.setRoles(Arrays.asList("USER"));
             userRepository.save(user);
-            return  true;
+            return true;
         } catch (Exception e) {
-           e.printStackTrace();
-           return false;
+//            log.warn("this is working of the logger{} " , user.getUserName(),e);
+            e.printStackTrace();
+            log.info("hahhahahah");
+            log.warn("hahhahahah");
+            log.debug("hahhahahah");
+            log.trace("hahhahahah");
+            log.error("hahhahahah");
+            return false;
         }
     }
 
